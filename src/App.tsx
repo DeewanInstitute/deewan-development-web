d002a45c025b305c57ba1e51f8e69b1cc36edaf9
Deewan Institute <app@deewaninstitute.com>
2026-07-28 16:03:12 +0300

    July 28 - Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <section id="coming-soon">
+      <img className="logo" src="/assets/images/logo.png" alt="Coming Soon" />
+      <h1>Deewan Development</h1>
+      <p>
+        We're building something new. Check back soon to see what we've been
+        working on.
+      </p>
+    </section>
+  )
+}
+
+export default App
