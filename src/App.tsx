1106c6021b4d36cf0420f5fed972921c9e1ba38d
Deewan Institute <app@deewaninstitute.com>
2026-07-29 17:59:46 +0300

    July, 29 -- Saba -- Assets, Fonts, Images, Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import './App.css'
+import Home from './pages/home'
 
 function App() {
   return (
-    <section id="coming-soon">
-      <img className="logo" src="/assets/images/logo.png" alt="Coming Soon" />
-      <h1>Deewan Development</h1>
-      <p>
-        We're building something new. Check back soon to see what we've been
-        working on.
-      </p>
-    </section>
+   <Home/>
   )
 }
 
